c15f5ef9d7d49f6345275ffb0cf7d65bc5df2871
tu1511 <131641671+tu1511@users.noreply.github.com>
2024-09-17 00:20:04 +0700

    Cai dat express chay thong bao xin chao

---
+++ server.js
@@ -0,0 +1,25 @@
+const app = require("./app");
+const config = require("./app/config");
+
+// start server
+const PORT = config.app.port;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
+
+// async function startServer() {
+//   try {
+//     await MongoDB.connect(config.db.uri);
+//     console.log("Connected to the database!");
+
+//     const PORT = config.app.port;
+//     app.listen(PORT, () => {
+//       console.log(`Serve is running on port ${PORT}`);
+//     });
+//   } catch (error) {
+//     console.log("Cannot connect to the database!", error);
+//     process.exit();
+//   }
+// }
+
+// startServer();
